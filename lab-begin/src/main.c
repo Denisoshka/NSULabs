53362556feb96ac927dc0b0b55f203dd2db653e6
g.timofeev <g.timofeev@g.nsu.ru>
2022-06-07 03:00:59 +0000

    Update 1.0

--- lab-begin/src/main.c
+++ lab-begin/src/main.c
@@ -1,5 +1,44 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
 
-int main(void) {
-    return EXIT_SUCCESS;
+int get_values(int length, int* array);
+void sum_array(int length, int* array);
+
+int main(){
+    int length;
+    if(scanf("%d", &length) != 1 || length < 1 || length > 10){
+        printf("bad input");
+        return 0;
+    }
+    int* array = (int*)(calloc(length, sizeof(int)));
+    if(get_values(length, array)){
+        sum_array(length, array);
+    }
+    free(array);
+    return 0;
+}
+
+int get_values(int length, int* array){
+    long long tmp;
+    for(int i = 0; i < length; i++){
+        if(scanf("%lld", &tmp) < 1 || tmp > INT_MAX || tmp < INT_MIN){
+            printf("bad input");
+            return 0;
+        }
+        array[i] = (int)tmp;
+    }
+    return 1;
+}
+
+void sum_array(int length, int* array){
+    long long int sum = 0;
+    for(int i = 0; i < length; i++){
+        sum += array[i];
+        if(sum > INT_MAX || sum < INT_MIN){
+            printf("overflow");
+            return;
+        }
+    }
+    printf("%d", (int)sum);
 }
