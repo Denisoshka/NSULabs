a5e948b06406635f8e2d09740783a56d564bb38e
d.zhdanov <d.zhdanov@g.nsu.ru>
2022-10-01 16:46:22 +0000

    Update main.c

--- lab2/src/main.c
+++ lab2/src/main.c
@@ -3,9 +3,9 @@
 
 const char H_zero = '0', H_nine = '9';
 
-char * scan_char( size_t * len_link )
+char * scan_char(int * len_link )
 {
-    size_t string_len = 1;
+    int string_len = 1;
 
     char symbol;
     char * string = malloc( sizeof(char) );
@@ -23,16 +23,16 @@ char * scan_char( size_t * len_link )
 }
 
 
-int correct_string( const char * string, size_t string_len)
+int correct_string( const char * string, int string_len)
 {
     int arr [10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    for( size_t index = 0; index < string_len; index++ )
+    for( int index = 0; index < string_len; index++ )
     {
         if ( H_zero <= string [index ] && string[ index ] <= H_nine )
             arr[ string[ index ] - H_zero ]++;
         else return 0;
     }
-    for( size_t index = 0; index < string_len; index++ )
+    for( int index = 0; index < string_len; index++ )
     {
         if( arr[ index ] > 1)
         {
@@ -42,10 +42,10 @@ int correct_string( const char * string, size_t string_len)
     return 1;
 }
 
-size_t find_first_necessary_index( const char * string, size_t string_len, size_t * index_link )
+int find_first_necessary_index( const char * string, int string_len )
 {
-    size_t first_necessary_index = 0 , proof_of_start = 0;
-    for ( size_t first_index = 0; first_index < string_len - 1; first_index++ )
+    int first_necessary_index = -1 , proof_of_start = 0;
+    for ( int first_index = 0; first_index < string_len - 1; first_index++ )
     {
         if ( string[ first_index ] < string[ first_index + 1])
         {
@@ -53,25 +53,23 @@ size_t find_first_necessary_index( const char * string, size_t string_len, size_
             proof_of_start++;
         }
     }
-    *index_link = first_necessary_index;
-    return ( proof_of_start  ) ? 1 : 0;
+    return first_necessary_index;
 }
 
-size_t find_second_necessary_index( const char * string, size_t string_len, size_t * index_link, size_t first_necessary_index )
+int find_second_necessary_index( const char * string, int string_len, int first_necessary_index )
 {
-    size_t second_necessary_index = 0;
-    for ( size_t second_index = first_necessary_index + 1; second_index < string_len; second_index++)
+    int second_necessary_index = 0;
+    for ( int second_index = first_necessary_index + 1; second_index < string_len; second_index++)
     {
         if ( string[ first_necessary_index ] < string[ second_index ])
         {
             second_necessary_index = second_index;
         }
     }
-    * index_link = second_necessary_index;
-    return 0;
+    return second_necessary_index;
 }
 
-void swap_symbols(char * string, const size_t string_len, const size_t first_necessary_index, const size_t second_necessary_index)
+void swap_symbols(char * string, const int string_len, const int first_necessary_index, const int second_necessary_index)
 {
     char sub_char;
 
@@ -79,8 +77,8 @@ void swap_symbols(char * string, const size_t string_len, const size_t first_nec
     string[ first_necessary_index ] = string[ second_necessary_index ];
     string[ second_necessary_index ] = sub_char;
 
-    size_t index = 0;
-    size_t front_index = 0, back_index = 0;
+    int index = 0;
+    int front_index, back_index;
     const size_t half_of_tail = ( string_len - ( first_necessary_index + 1 ) ) / 2;
 
     for(; index < half_of_tail; index++  )
@@ -94,17 +92,17 @@ void swap_symbols(char * string, const size_t string_len, const size_t first_nec
     }
 }
 
-void print_permutation( char * string, size_t string_len, size_t permutation_quantity)
+void print_permutation( char * string, int string_len, int permutation_quantity)
 {
-    for(size_t iteration = 0; iteration < permutation_quantity; iteration++)
+    for(int iteration = 0; iteration < permutation_quantity; iteration++)
     {
-        size_t proof_of_start;
-        size_t first_necessary_index = 0, second_necessary_index = 0;
+        int proof_of_start;
+        int first_necessary_index = 0, second_necessary_index = 0;
 
-        proof_of_start = find_first_necessary_index( string, string_len, &first_necessary_index );
-        find_second_necessary_index( string, string_len, &second_necessary_index, first_necessary_index );
+        first_necessary_index = find_first_necessary_index( string, string_len );
+        second_necessary_index = find_second_necessary_index( string, string_len, first_necessary_index );
 
-        if ( proof_of_start == 0)
+        if ( first_necessary_index == -1 )
         {
             return;
         }
@@ -117,14 +115,12 @@ void print_permutation( char * string, size_t string_len, size_t permutation_qua
 
 int main(void) {
 
-    size_t permutations_quantity,
-            main_string_len,
-            count_of_scan;
+    int permutations_quantity, main_string_len, count_of_scan;
 
     char *main_string;
 
     main_string = scan_char(&main_string_len);
-    count_of_scan = scanf("%lu", &permutations_quantity);
+    count_of_scan = scanf("%d", &permutations_quantity);
 
     if ( !(correct_string(main_string, main_string_len) ) )
     {
