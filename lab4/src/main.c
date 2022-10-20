1d380ce4e8902227304cc698f0731a198ac92586
d.zhdanov <d.zhdanov@g.nsu.ru>
2022-10-20 10:56:26 +0000

    Update main.c

--- lab4/src/main.c
+++ lab4/src/main.c
@@ -1,7 +1,7 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 const char actions_list[] = {'+', '-', '/', '*', '\0'};
 
@@ -63,7 +63,7 @@ int prepare_string_vector(symbol_vector *vector)
 {
 //    FILE * thread_in = fopen( "C:\\Users\\dinis\\CLionProjects\\lab4\\in", "r");
     FILE * thread_in = fopen( "in.txt", "r");
-    if ( fscanf( thread_in,"%[^\n]s", vector->array) != 1)
+    if ( fscanf( thread_in,"%1000[^\n]s", vector->array) != 1)
     {
         return SYNTAX_ERROR;
     }
@@ -232,17 +232,6 @@ int do_when_end_bracket( symbol_vector * char_stack, int_vector * int_stack ) {
 
 int do_when_action(symbol_vector * char_stack, int_vector * int_stack, char action, int action_index)
 {
-    char old_action;
-    int old_action_index,
-
-            action_priority,
-            old_action_priority,
-
-    old_number,
-    new_number,
-
-            result;
-
     if(is_empty_char_stack( char_stack ))
     {
         push_char_stack( char_stack, action );
@@ -251,7 +240,7 @@ int do_when_action(symbol_vector * char_stack, int_vector * int_stack, char acti
     }
     else
     {
-        old_action = get_char_stack( char_stack );
+        char old_action = get_char_stack( char_stack );
 
         if ( old_action == ')' )
         {
@@ -264,10 +253,10 @@ int do_when_action(symbol_vector * char_stack, int_vector * int_stack, char acti
             return MOVE_AWAY;
         }
 
-        old_action_index = get_index_in_list( actions_list, old_action );
+        int old_action_index = get_index_in_list( actions_list, old_action );
 
-        action_priority = actions_priority[ action_index];
-        old_action_priority = actions_priority[ old_action_index ];
+        int action_priority = actions_priority[ action_index];
+        int old_action_priority = actions_priority[ old_action_index ];
 
         if ( old_action_priority >= action_priority )
         {
@@ -276,15 +265,15 @@ int do_when_action(symbol_vector * char_stack, int_vector * int_stack, char acti
             {
                 return SYNTAX_ERROR;
             }
-            new_number = pop_int_stack( int_stack );
-            old_number = pop_int_stack( int_stack );
+            int new_number = pop_int_stack( int_stack );
+            int old_number = pop_int_stack( int_stack );
 
             if ( old_action == '/' && new_number == 0 )
             {
                 return DIVISION_BY_ZERO;
             }
 
-            result = do_action( old_number, new_number, old_action);
+            int result = do_action( old_number, new_number, old_action);
             push_int_stack( int_stack, result );
 
             return STAY;
@@ -301,14 +290,9 @@ int do_when_action(symbol_vector * char_stack, int_vector * int_stack, char acti
 
 int final_calculations( int_vector * int_stack, symbol_vector * char_stack )
 {
-    int new_number,
-        old_number,
-        result;
-    char action;
-
     while ( !is_empty_char_stack(char_stack) )
     {
-        action = pop_char_stack( char_stack);
+        char action = pop_char_stack( char_stack);
         if ( int_stack -> array_len < 2 )
         {
             return SYNTAX_ERROR;
@@ -318,14 +302,14 @@ int final_calculations( int_vector * int_stack, symbol_vector * char_stack )
             return SYNTAX_ERROR;
         }
 
-        new_number = pop_int_stack( int_stack);
-        old_number = pop_int_stack( int_stack);
+        int new_number = pop_int_stack( int_stack);
+        int old_number = pop_int_stack( int_stack);
 
         if (action == '/' && new_number == 0 )
         {
             return DIVISION_BY_ZERO;
         }
-        result = do_action( old_number, new_number, action );
+        int result = do_action( old_number, new_number, action );
         push_int_stack( int_stack, result);
     }
     if (  char_stack->array_len == 0 && int_stack->array_len == 1)
@@ -346,15 +330,12 @@ int count_expression(symbol_vector * string_vector, symbol_vector * char_stack, 
             action_index,
             flag = 0,
 
-        is_digit,
         returned_result;
-
-    char symbol;
 
     for (int index = 0 ; index < string_len ; )
     {
-        symbol = string[index];
-        is_digit = '0' <= symbol && symbol <= '9';
+        char symbol = string[index];
+        int is_digit = '0' <= symbol && symbol <= '9';
 
         if (is_digit)
         {
