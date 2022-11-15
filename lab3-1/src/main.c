2a964b674f8ff3197fa2dc1555383ddc1444cbcc
d.zhdanov <d.zhdanov@g.nsu.ru>
2022-11-15 17:37:23 +0000

    Update main.c

--- lab3-1/src/main.c
+++ lab3-1/src/main.c
@@ -1,90 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_error_on_line(int line, int * flag )
-{
-    printf( "__LINE__ %d", line);
-    * flag = 1;
-}
-
-typedef struct ARRAY_vector
+typedef struct our_custom_array
 {
     int * array;
     int array_len;
-}ARRAY_vector;
+}our_custom_array;
 
-ARRAY_vector create_vector(void)
+our_custom_array create_custom_array(const int array_size )
 {
-    ARRAY_vector vector = {
-            .array = NULL,
-            .array_len = 0,
+    our_custom_array custom_array = {
+            .array = malloc(sizeof( int ) * array_size ),
+            .array_len = array_size,
     };
-    return vector;
+    return custom_array;
 }
 
-void destroy_vector( ARRAY_vector * vector )
+void destroy_custom_array(our_custom_array * custom_array )
 {
-    free( vector->array );
+    free(custom_array->array );
+    custom_array->array = NULL;
 }
 
-void prepare_vector( ARRAY_vector * vector, int * flag)
+int prepare_custom_array(our_custom_array * custom_array)
 {
+    int scanned_array_len;
+
     FILE * thread_in = fopen( "in.txt", "r");
     if ( thread_in == NULL)
     {
-        print_error_on_line(__LINE__, flag);
-        return;
+        printf( "__LINE__ %d", __LINE__);
+        return 1;
     }
 
-    if ( fscanf(thread_in,"%d", &vector->array_len) != 1 )
+    if ( fscanf(thread_in,"%d", &scanned_array_len) != 1 )
     {
-        print_error_on_line(__LINE__, flag);
+        printf( "__LINE__ %d", __LINE__);
         fclose( thread_in);
-        return;
+        return 1;
     }
 
-    vector -> array = malloc( vector->array_len * sizeof( int ) );
-    if ( vector->array == NULL)
+    * custom_array = create_custom_array(scanned_array_len);
+    if (custom_array->array == NULL)
     {
-        print_error_on_line(__LINE__, flag);
+        printf( "__LINE__ %d", __LINE__);
         fclose( thread_in);
-        return;
+        return 1;
     }
 
-    for (int index = 0; index < vector->array_len; index++)
+    for (int index = 0; index < custom_array->array_len; index++)
     {
-        if( fscanf( thread_in,"%d", &vector->array[index]) != 1)
+        if(fscanf( thread_in,"%d", &custom_array->array[index]) != 1)
         {
-            print_error_on_line(__LINE__, flag);
+            printf( "__LINE__ %d", __LINE__);
             fclose( thread_in);
-            return;
+            return 1;
         }
     }
     fclose(thread_in);
+    return 0;
 }
 
-void swap_elements( int * first_element, int * second_element)
+void swap( int * first_element, int * second_element)
 {
     int sub_element = * first_element;
     * first_element = * second_element;
     * second_element = sub_element;
 }
 
-void quick_sort(ARRAY_vector * vector, const int begin_index, const int end_index)
+void quick_sort(our_custom_array * custom_array, const int begin_index, const int end_index)
 {
-    int first_index = begin_index;
-    int second_index = end_index;
-
-    int patrition_element = vector->array[ (begin_index + end_index) / 2 ];
+    int first_index = begin_index,
+        second_index = end_index;
+    const int patrition_element = custom_array->array[(begin_index + end_index) / 2 ];
 
     for ( ; first_index < second_index; )
     {
-        for ( ; ( first_index < vector->array_len ) && vector->array[first_index] < patrition_element; first_index++);
-        for ( ; ( second_index >= 0 ) && patrition_element < vector->array[second_index] ; second_index--);
+        for ( ; (first_index < custom_array->array_len ) && custom_array->array[first_index] < patrition_element; first_index++);
+        for ( ; ( second_index >= 0 ) && patrition_element < custom_array->array[second_index] ; second_index--);
 
         if (first_index <= second_index)
         {
-            swap_elements( &vector->array[first_index], &vector->array[second_index]);
+            swap(&custom_array->array[first_index], &custom_array->array[second_index]);
 
             first_index++;
             second_index--;
@@ -92,40 +89,45 @@ void quick_sort(ARRAY_vector * vector, const int begin_index, const int end_inde
     }
     if ( begin_index < second_index )
     {
-        quick_sort(vector, begin_index, second_index);
+        quick_sort(custom_array, begin_index, second_index);
     }
     if ( first_index < end_index )
     {
-        quick_sort(vector, first_index, end_index);
+        quick_sort(custom_array, first_index, end_index);
     }
 }
 
-void print_array( const ARRAY_vector * vector )
+int print_array( const our_custom_array * vector )
 {
     FILE * thread_out = fopen("out.txt", "w");
+    if ( thread_out == NULL )
+    {
+        printf( "__LINE__ %d", __LINE__);
+        return 1;
+    }
     for (int index = 0; index < vector->array_len; index++)
     {
-        if( -1 == fprintf(thread_out, "%d ", vector->array[index]))
+        if( fprintf(thread_out, "%d ", vector->array[index]) == -1  )
         {
             printf( "__LINE__ %d", __LINE__);
-            break;
+            fclose( thread_out );
+            return 1;
         }
     }
     fclose( thread_out );
+    return 0;
 }
 
 int main( void )
 {
-    int flag = 0;
-    
-    ARRAY_vector vector = create_vector();
-    prepare_vector( &vector, &flag);
-    if (flag != 0)
+    our_custom_array vector;
+
+    if ( prepare_custom_array(&vector) )
     {
-        destroy_vector( &vector);
+        destroy_custom_array(&vector);
         return 0;
     }
-    
+
     if (vector.array_len > 1)
     {
         quick_sort( &vector, 0, vector.array_len - 1);
@@ -133,7 +135,7 @@ int main( void )
 
     print_array( &vector);
 
-    destroy_vector( &vector);
-    
+    destroy_custom_array(&vector);
+
     return 0;
 }
