bba13427a5c9605f8ed039b1b685dd593fc487ff
d.zhdanov <d.zhdanov@g.nsu.ru>
2022-11-15 19:28:33 +0000

    Update main.c

--- lab1-1/src/main.c
+++ lab1-1/src/main.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 
 const int K_max_pattern_len = 16,
-     K_max_chunk_len = 1024,
-     K_hash_base = 3;
+        K_max_chunk_len = 1024,
+        K_hash_base = 3;
 
-typedef struct vector{
+typedef struct our_custom_array{
     unsigned char * array;
     int array_len;
     int additional;
     int hash;
-}vector;
+}our_custom_array;
 
-vector create_vector( const int necessary_array_len )
+our_custom_array create_custom_array( const int necessary_array_len )
 {
-    vector blank = {
+    our_custom_array blank = {
             .array = malloc( sizeof(unsigned char) * necessary_array_len ),
             .array_len = necessary_array_len,
             .additional = 0,
@@ -22,25 +22,36 @@ vector create_vector( const int necessary_array_len )
     };
     return blank;
 }
-void destroy_vector( vector * blank )
+void destroy_custom_array(our_custom_array * blank )
 {
     free( blank->array );
     blank->array = NULL;
 }
 
-void scan_pattern( vector * pattern, FILE * thread_in )
+int scan_pattern(our_custom_array * pattern, FILE * stream_in )
 {
     int index = 0;
     unsigned char symbol;
 
-    for ( ; ( 1 == fread( &symbol, sizeof(unsigned char), 1, thread_in ) ) && symbol != '\n'; index++)
+    for ( ; ; index++)
     {
+        if( 1 != fread(&symbol, sizeof(unsigned char), 1, stream_in ) )
+        {
+            fprintf( stderr, "__LINE__ %d\n", __LINE__);
+            return 1;
+        }
+
+        if ( symbol == '\n' )
+        {
+            break;
+        }
         pattern->array[index] = symbol;
     }
     pattern->array_len = index;
+    return 0;
 }
 
-int get_hash(vector * blank, const vector * pattern)
+int get_hash(our_custom_array * blank, const our_custom_array * pattern)
 {
     int degree = 1;
     for( int index  = 0 ; index < pattern->array_len; index++, degree *= K_hash_base)
@@ -50,7 +61,7 @@ int get_hash(vector * blank, const vector * pattern)
     return degree/3;
 }
 
-void text_rewrite( vector * text, const vector * pattern, FILE * thread_in )
+void text_rewrite(our_custom_array * text, const our_custom_array * pattern, FILE * stream_in )
 {
     int where_start_rewrite = text->additional - pattern->array_len + 1;
     int iterations_quantity = pattern->array_len - text->additional + text->array_len - 1;
@@ -59,11 +70,11 @@ void text_rewrite( vector * text, const vector * pattern, FILE * thread_in )
     {
         text->array[iteration] = text->array[where_start_rewrite];
     }
-    text -> array_len = (int)fread( text->array + iterations_quantity, sizeof( unsigned char), text->array_len - iterations_quantity, thread_in) + iterations_quantity;
+    text -> array_len = (int)fread( text->array + iterations_quantity, sizeof( unsigned char), text->array_len - iterations_quantity, stream_in) + iterations_quantity;
     text->additional = pattern->array_len - 1;
 }
 
-void update_hash_and_get(vector * text, const vector * pattern, const int flag )
+void update_hash_and_get(our_custom_array * text, const our_custom_array * pattern, const int flag )
 {
     if (flag)
     {
@@ -77,13 +88,13 @@ void update_hash_and_get(vector * text, const vector * pattern, const int flag )
     text->hash += (text->array[text->additional] % K_hash_base) * pattern->additional;
 }
 
-void search_substring(vector * text, const vector * pattern, const int * total_index, FILE * thread_out)
+void search_substring(our_custom_array * text, const our_custom_array * pattern, const int * total_index, FILE * stream_out)
 {
     int print_index = * total_index - pattern->array_len + 1;
 
     for( int pattern_work_index = 0, text_work_index = text->additional - pattern->array_len + 1; pattern_work_index < pattern->array_len; pattern_work_index++, text_work_index++)
     {
-        fprintf( thread_out, "%d ", print_index + pattern_work_index);
+        fprintf(stream_out, "%d ", print_index + pattern_work_index);
 
         if( text->array[ text_work_index ] != pattern->array[ pattern_work_index ] )
         {
@@ -92,37 +103,39 @@ void search_substring(vector * text, const vector * pattern, const int * total_i
     }
 }
 
-void rabin_carp_algorithm(const vector * pattern, FILE * thread_in)
+int rabin_carp_algorithm(const our_custom_array * pattern, FILE * stream_in)
 {
-    vector text = create_vector( K_max_chunk_len );
+    our_custom_array text = create_custom_array(K_max_chunk_len);
     if ( text.array == NULL)
     {
-        printf("__LINE__ %d", __LINE__);
-        return;
+        fprintf( stderr, "__LINE__ %d\n", __LINE__);
+        return 1;
     }
-    FILE * thread_out = fopen( "out.txt", "w");
-    if ( thread_out == NULL)
+
+    FILE * stream_out = fopen("out.txt", "w");
+    if (stream_out == NULL)
     {
-        destroy_vector( &text );
-        printf("__LINE__ %d", __LINE__);
-        return;
+        destroy_custom_array(&text);
+        fprintf( stderr, "__LINE__ %d\n", __LINE__);
+        return 1;
     }
-    
+
     int total_index = pattern->array_len;
     text.additional = pattern->array_len - 1;
-    text.array_len = (int)fread( text.array, sizeof (unsigned char ), K_max_chunk_len, thread_in);
+    text.array_len = (int)fread(text.array, sizeof (unsigned char ), K_max_chunk_len, stream_in);
 
-    fprintf( thread_out, "%d ", pattern->hash);
+    fprintf(stream_out, "%d ", pattern->hash);
 
     if (pattern->array_len <= text.array_len)
     {
         get_hash( &text, pattern);
     }
+
     while (pattern->array_len <= text.array_len)
     {
         if (pattern->hash == text.hash)
         {
-            search_substring( &text, pattern, &total_index,  thread_out);
+            search_substring(&text, pattern, &total_index, stream_out);
         }
 
         total_index++;
@@ -130,7 +143,7 @@ void rabin_carp_algorithm(const vector * pattern, FILE * thread_in)
 
         if ( text.additional >= text.array_len )
         {
-            text_rewrite(&text, pattern, thread_in);
+            text_rewrite(&text, pattern, stream_in);
             update_hash_and_get(&text, pattern, 0);
         }
         else
@@ -138,35 +151,43 @@ void rabin_carp_algorithm(const vector * pattern, FILE * thread_in)
             update_hash_and_get(&text, pattern, 1);
         }
     }
-    
-    fclose( thread_out);
-    destroy_vector( &text);
+
+    fclose(stream_out);
+    destroy_custom_array(&text);
+
+    return 0;
 }
 
 int main(void)
 {
-    FILE * thread_in = fopen( "in.txt", "r");
-    if ( thread_in == NULL )
+    FILE * stream_in = fopen("in.txt", "r");
+    if (stream_in == NULL )
     {
-        printf("__LINE__ %d", __LINE__);
+        fprintf( stderr, "__LINE__ %d\n", __LINE__);
         return 0;
     }
-    vector pattern = create_vector( K_max_pattern_len );
+
+    our_custom_array pattern = create_custom_array(K_max_pattern_len);
     if ( pattern.array == NULL)
     {
-        fclose( thread_in );
-        printf("__LINE__ %d", __LINE__);
+        fclose(stream_in );
+        fprintf( stderr, "__LINE__ %d\n", __LINE__);
         return 0;
     }
-    
 
-    scan_pattern( &pattern, thread_in);
+    if ( scan_pattern(&pattern, stream_in) )
+    {
+        fclose(stream_in );
+        destroy_custom_array( &pattern );
+        fprintf( stderr, "__LINE__ %d\n", __LINE__);
+        return 0;
+    }
     pattern.additional = get_hash( &pattern, &pattern);
-    
-    rabin_carp_algorithm( &pattern, thread_in );
 
-    fclose( thread_in );
-    destroy_vector( &pattern );
-    
+    rabin_carp_algorithm(&pattern, stream_in );
+
+    fclose(stream_in );
+    destroy_custom_array(&pattern);
+
     return 0;
 }
